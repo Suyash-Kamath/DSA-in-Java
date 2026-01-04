1f4e3e8de50e868875c7ecf4d86ae9c9399109c6
Suyash Kamath <suyashkamath9@gmail.com>
2026-01-04 21:10:56 +0530

    Left Rotate by One

--- arrays/LeftRotateAnArrayByOne.java
+++ arrays/LeftRotateAnArrayByOne.java
@@ -1,4 +1,24 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class LeftRotateAnArrayByOne {
+
+    public static void leftRotate(int[] nums) {
+        int temp = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i - 1] = nums[i];
+        }
+
+        nums[nums.length - 1] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+
+        leftRotate(nums);
+
+        System.out.println("Rotated Array by 1: " + Arrays.toString(nums));
+    }
 }
