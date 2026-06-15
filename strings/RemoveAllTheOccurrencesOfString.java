c469180fa6af275ec3e245e171e522335bb94c44
Suyash Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2026-06-15 09:39:37 +0530

    Create RemoveAllTheOccurrencesOfString.java

---
+++ strings/RemoveAllTheOccurrencesOfString.java
@@ -0,0 +1,19 @@
+public class RemoveAllTheOccurrencesOfString {
+    static String removeOccurrences(String s,String part){
+        // 
+        while(s.contains(part)){
+            int index = s.indexOf(part); // left most ka index dega , lets say part is pq , so it will gice me the index of p 
+            // create a new string by merging left and right remnant
+            
+            s = s.substring(0,index) + s.substring(index+part.length());
+            
+        }
+        return s;
+    }
+    public static void main(String[] args) {
+        String s = "pqxpqpqyzpq";
+        String part = "pq";
+        String ans = removeOccurrences(s,part);
+        System.out.println(ans);
+    }
+}
