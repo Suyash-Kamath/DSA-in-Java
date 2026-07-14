e013a96afdd6b2b19f92edf7967ae4473c4f584b
Suyash Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2026-07-14 17:28:28 +0530

    Update CombinationSumIII.java

--- recursion/CombinationSumIII.java
+++ recursion/CombinationSumIII.java
@@ -1,34 +1,120 @@
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class CombinationSumIII {
+
+//     static void solve(int number,
+//                       int k,
+//                       int target,
+//                       List<List<Integer>> ans,
+//                       List<Integer> output) {
+
+//         // Base Case:
+//         // We selected exactly k numbers.
+//         if (output.size() == k) {
+
+//             // If target also becomes 0,
+//             // then we found one valid answer.
+//             if (target == 0) {
+//                 ans.add(new ArrayList<>(output));
+//             }
+
+//             return;
+//         }
+
+//         // Numbers are only from 1 to 9.
+//         if (number > 9) {
+//             return;
+//         }
+
+//         // Target became negative.
+//         if (target < 0) {
+//             return;
+//         }
+
+//         // --------------------------------
+//         // Choice 1 : Take current number
+//         // --------------------------------
+
+//         output.add(number);
+
+//         solve(
+//                 number + 1,
+//                 k,
+//                 target - number,
+//                 ans,
+//                 output
+//         );
+
+//         // Backtrack
+//         output.remove(output.size() - 1);
+
+//         // --------------------------------
+//         // Choice 2 : Skip current number
+//         // --------------------------------
+
+//         solve(
+//                 number + 1,
+//                 k,
+//                 target,
+//                 ans,
+//                 output
+//         );
+//     }
+
+//     static List<List<Integer>> combinationSumIII(int k, int n) {
+
+//         List<List<Integer>> ans = new ArrayList<>();
+//         List<Integer> output = new ArrayList<>();
+
+//         solve(1, k, n, ans, output);
+
+//         return ans;
+//     }
+
+//     public static void main(String[] args) {
+
+//         int k = 3;
+//         int n = 9;
+
+//         List<List<Integer>> ans = combinationSumIII(k, n);
+
+//         for (List<Integer> combination : ans) {
+//             System.out.println(combination);
+//         }
+//     }
+// }
+
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CombinationSumIII {
 
-    static void solve(int number,
-                      int k,
+    static void solve(int[] candidates,
                       int target,
+                      int k,
+                      int index,
                       List<List<Integer>> ans,
                       List<Integer> output) {
 
-        // Base Case:
-        // We selected exactly k numbers.
-        if (output.size() == k) {
-
-            // If target also becomes 0,
-            // then we found one valid answer.
-            if (target == 0) {
-                ans.add(new ArrayList<>(output));
-            }
-
+        // Target achieved
+        if (target == 0 && output.size() == k) {
+            ans.add(new ArrayList<>(output));
             return;
         }
 
-        // Numbers are only from 1 to 9.
-        if (number > 9) {
+        // Invalid cases
+        if (index >= candidates.length) {
             return;
         }
 
-        // Target became negative.
         if (target < 0) {
+            return;
+        }
+
+        if (output.size() > k) {
             return;
         }
 
@@ -36,12 +122,15 @@ public class CombinationSumIII {
         // Choice 1 : Take current number
         // --------------------------------
 
-        output.add(number);
+        output.add(candidates[index]);
 
+        // Move to next index because
+        // every number can be used only once
         solve(
-                number + 1,
+                candidates,
+                target - candidates[index],
                 k,
-                target - number,
+                index + 1,
                 ans,
                 output
         );
@@ -54,20 +143,32 @@ public class CombinationSumIII {
         // --------------------------------
 
         solve(
-                number + 1,
-                k,
+                candidates,
                 target,
+                k,
+                index + 1,
                 ans,
                 output
         );
     }
 
-    static List<List<Integer>> combinationSumIII(int k, int n) {
+    static List<List<Integer>> combinationSumIII(int k, int target) {
+
+        int[] candidates = {
+                1,2,3,4,5,6,7,8,9
+        };
 
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> output = new ArrayList<>();
 
-        solve(1, k, n, ans, output);
+        solve(
+                candidates,
+                target,
+                k,
+                0,
+                ans,
+                output
+        );
 
         return ans;
     }
@@ -75,9 +176,9 @@ public class CombinationSumIII {
     public static void main(String[] args) {
 
         int k = 3;
-        int n = 9;
+        int target = 9;
 
-        List<List<Integer>> ans = combinationSumIII(k, n);
+        List<List<Integer>> ans = combinationSumIII(k, target);
 
         for (List<Integer> combination : ans) {
             System.out.println(combination);
